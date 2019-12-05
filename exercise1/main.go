26b1a048a36bdd31668e62a7d779cee5b9a6d7dc
voje <voje@iskratel.si>
2019-12-05 17:06:19 +0100

    generic commit message

--- exercise1/main.go
+++ exercise1/main.go
@@ -8,12 +8,23 @@ import (
 
 // FuelCounterUpper counts up the needed fuel.
 type FuelCounterUpper struct {
-	r         io.Reader
-	FuelCount int
+	r io.Reader
+}
+
+// LineReader reads an input streams and outputs line-per-line.
+// (Yes, I am aware of the bufio package.)
+type LineReader struct {
+	r io.Reader
+}
+
+// Read has a caveat. If line length exceeds buffer length, the program will have undefined behaviour.
+func (lr *LineReader) Read(b []byte) (n int, err error) {
+	lineBuffer := buf
+	n, err := lr.r.Read(buf)
 }
 
 func main() {
 	file, _ := os.Open("data.txt")
 	fcu := FuelCounterUpper{r: file}
-	fmt.Println(fcu.FuelCount)
+	fmt.Println(fcu.CalcFuel())
 }
