e2274b3386aeb23eb66c192ccde54e05a7d33450
voje <voje@iskratel.si>
2019-12-06 11:43:54 +0100

    exercise1

--- exercise1/main.go
+++ exercise1/main.go
@@ -1,30 +1,67 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
+	"math"
 	"os"
+	"strconv"
 )
 
-// FuelCounterUpper counts up the needed fuel.
-type FuelCounterUpper struct {
-	r io.Reader
+// CalcFuel calculates the mass of fuel needed to carry m.
+// Negative fuel gets converted to 0.
+func CalcFuel(mass int) (fuelMass int) {
+	fuelMass = int(math.Floor(float64(mass)/3) - 2)
+	if fuelMass < 0 {
+		return 0
+	}
+	return
 }
 
-// LineReader reads an input streams and outputs line-per-line.
-// (Yes, I am aware of the bufio package.)
-type LineReader struct {
-	r io.Reader
+// CalcFuelRecHelper calculates the mass of fuel needed to carry m, adding the
+// fueld needed to carry the extra fuel.
+func CalcFuelRecHelper(mass int) int {
+	if mass == 0 {
+		return 0
+	}
+	return mass + CalcFuelRecHelper(CalcFuel(mass))
 }
 
-// Read has a caveat. If line length exceeds buffer length, the program will have undefined behaviour.
-func (lr *LineReader) Read(b []byte) (n int, err error) {
-	lineBuffer := buf
-	n, err := lr.r.Read(buf)
+// CalcFuelRec calculates the mass of needed fuel and subtracts the initial item mass.
+func CalcFuelRec(mass int) int {
+	return CalcFuelRecHelper(mass) - mass
 }
 
 func main() {
 	file, _ := os.Open("data.txt")
-	fcu := FuelCounterUpper{r: file}
-	fmt.Println(fcu.CalcFuel())
+	scanner := bufio.NewScanner(file)
+
+	// Sum the fuel: floor(mass/3) - 2
+	var sum int = 0
+	for scanner.Scan() {
+		sItemMass := scanner.Text()
+		iItemMass, err := strconv.Atoi(sItemMass)
+		if err != nil {
+			panic(err)
+		}
+		sum += CalcFuel(iItemMass)
+	}
+	fmt.Printf("Simple fuel summary: %d\n", sum)
+
+	// Reset reader.
+	file.Seek(0, io.SeekStart)
+	scanner = bufio.NewScanner(file)
+
+	// Sum the fuel as above, recursively adding fuel weight until negative.
+	var recSum int = 0
+	for scanner.Scan() {
+		sItemMass := scanner.Text()
+		iItemMass, err := strconv.Atoi(sItemMass)
+		if err != nil {
+			panic(err)
+		}
+		recSum += CalcFuelRec(iItemMass)
+	}
+	fmt.Printf("Recursive fuel summary: %d\n", recSum)
 }
