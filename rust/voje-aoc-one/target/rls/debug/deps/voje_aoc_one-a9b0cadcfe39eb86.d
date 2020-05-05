499573dedf9012f545b6cfcc4acd01946566b949
voje <kristjan.voje@gmail.com>
2020-05-05 18:00:32 +0200

    rust

--- rust/voje-aoc-one/target/rls/debug/deps/voje_aoc_one-a9b0cadcfe39eb86.d
+++
@@ -1,5 +0,0 @@
-/c/Users/kristjan/git/aoc2019/rust/voje-aoc-one/target/rls/debug/deps/voje_aoc_one-a9b0cadcfe39eb86.rmeta: src/main.rs
-
-/c/Users/kristjan/git/aoc2019/rust/voje-aoc-one/target/rls/debug/deps/voje_aoc_one-a9b0cadcfe39eb86.d: src/main.rs
-
-src/main.rs:
